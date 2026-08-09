aa7e694182dc1fdc4948693ca24725ace0bed944
Atharv100906 <shinkaratharva06@gmail.com>
2026-08-09 17:35:49 +0530

    Added Assignment 4

---
+++ Assignment 4/script.js
@@ -0,0 +1,11 @@
+function calculateFactorial() {
+    let num = document.getElementById("num").value;
+    let factorial = 1;
+
+    for (let i = 1; i <= num; i++) {
+        factorial = factorial * i;
+    }
+
+    document.getElementById("result").innerHTML =
+        "Factorial = " + factorial;
+}
