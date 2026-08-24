33d3a8d6e90919d65710fc3d7da132f823eb3680
Atharv100906 <shinkaratharva06@gmail.com>
2026-08-24 20:57:23 +0530

    Add Assignment 4

--- Assignment 4/script.js
+++ Assignment 4/script.js
@@ -1,9 +1,6 @@
-const taskInput = document.getElementById("taskInput");
-const taskList = document.getElementById("taskList");
-
 function addTask() {
-
-    let task = taskInput.value;
+    let input = document.getElementById("taskInput");
+    let task = input.value;
 
     if (task === "") {
         alert("Please enter a task!");
@@ -12,18 +9,12 @@ function addTask() {
 
     let li = document.createElement("li");
 
-    li.innerHTML = `
-        <span>${task}</span>
-        <button class="delete" onclick="deleteTask(this)">Delete</button>
-    `;
+    li.innerHTML = task +
+        ' <button class="delete-btn" onclick="deleteTask(this)">Delete</button>';
 
-    li.querySelector("span").onclick = function() {
-        li.classList.toggle("completed");
-    };
+    document.getElementById("taskList").appendChild(li);
 
-    taskList.appendChild(li);
-
-    taskInput.value = "";
+    input.value = "";
 }
 
 function deleteTask(button) {
